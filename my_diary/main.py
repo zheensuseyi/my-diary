62912855c5781872c47679eaacb86ad665f066ca
Zheen <107953197+zheensuseyi@users.noreply.github.com>
2024-12-21 18:15:59 -0800

    Updated Main, added separate functions

--- my_diary/main.py
+++ my_diary/main.py
@@ -1,47 +1,9 @@
-# Zheen H. Suseyi
-# 10/28/24
-# my_diary
-
 import random
 from pathlib import Path
 
-def menu():
-    # Some fun ascii art representing our virtual assistant Soda!
-    art = """⢰⣾⣿⢶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣏⣿⣿
-⠸⣿⣿⣿⣇⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣿⣿⠿⠋
-⠀⠀⠉⢻⡅⠈⠉⠙⠓⠶⢦⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡤⠶⠚⠛⠉⠁⠠⣾⠇⠀⠀
-⠀⠀⠀⠈⣿⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠓⠲⠦⢤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠴⠶⠚⠋⠉⠁⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀
-⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠃⠀⠀⠀
-⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⣀⣀⣤⡴⠶⠶⠶⠶⠶⠦⣤⣀⣀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠉⠁⠀⠀⠂⠀⠀⠀⠀⠀⠒⠀⠉⠙⠛⢶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⠃⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠈⠛⢶⣄⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢀⣴⠶⠛⠉⠉⠉⠛⠷⣤⡀⠀⠀⠀⠀⠙⢷⣄⠀⡀⠀⠀⢸⠃⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⢡⣶⣶⡄⠀⠀⢠⡶⣮⢻⣆⠀⠀⠀⠀⠀⠻⣦⠀⠀⣸⡟⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣸⣿⣿⠃⠀⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠙⣷⠶⠿⠃⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠶⠶⣶⣶⡶⠄⠀⠀⠀⠀⠀⠀⠀⣀⡴⠾⠿⢯⣽⣿⣷⣤⢠⡆⠠⡄⣽⣿⣿⡿⠟⠛⢿⣆⠀⠀⠀⠻⣆⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡿⠁⠀⠀⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠉⠉⠻⠿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⢙⢷⣄⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⠀⢀⡴⠋⢀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣶⣶⡍⣈⣿⣆⠀⠼⣇⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⢰⡟⠁⢀⣀⣸⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⢀⠀⢀⠀⠀⠀⢸⣿⣿⣿⣿⡅⠀⢻⡆⠀⣿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡆⠀⠀⠀⢰⡟⠀⠀⠀⠀⣿⣿⣿⣿⣿⡇⠀⠀⢀⠄⡰⠋⡴⠃⡴⠁⠀⠘⣿⣿⣿⣿⡇⠀⠀⣿⡞⣿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣧⠀⠀⠀⣾⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⠃⠀⠀⢀⠞⠀⠊⢀⣈⣀⠀⠀⠀⠙⠿⠿⠟⠀⠀⠀⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣎⠀⠀⣿⠀⠀⠀⠀⠀⠀⠙⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠘⠻⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡂⣀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣾⢿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠖⠒⠒⠒⠒⠒⠒⠒⠒⠒⠶⠶⡦⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢯⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⣠⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣧⣄⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣷⠚⠁⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠛⢲⣴⣾⣿⡿⠿⣶⡶⠖⠉⠉⠉⠙⢦⠀⠀⠀⠀⠀⠀⣠⠞⠉⠙⠻⣿⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢶⠟⠁⢠⠏⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠙⣿⢿⡏⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⢸⠀⠛⠚⠃⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠈⠦⣀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠦⣄⣀⡀⣀⣠⠴⠊⠳⠶⠶⠶⠶⠶⠶⠤⠤⠤⠤⠼⢹⠒⠒⠋⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⠤⠤⠤⣤⠀⠀⠀⢀⣿⠀⠉⠁⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"""
-
-
-
-    # ability to exit out of the game loop
-    test_bool = False
-    # wisdom quotes, this will be shuffled once called
-    daily_wisdom = [
+# Define the required lists
+greetings = ['Hello!', 'Hi there!', 'Hey!', 'Greetings!']
+daily_wisdom = [
         "As you sow, so shall you reap.",
         "You get what you give.",
         "Life is a boomerang; what you send out, you get back.",
@@ -119,35 +81,88 @@ def menu():
         "We cannot solve our problems with the same thinking we used when we created them.",
         "Opportunities don't happen. You create them."
     ]
+def choice_one():
+    entry_title = input("Enter Entry Title (Only Use Letters For Your Title):\n")
+    if entry_title.isalpha():
+        random_greeting = random.choice(greetings)
+        desktop = Path.home() / 'Desktop'
+        folder_path = desktop / 'Diary Entries'
+        folder_path.mkdir(exist_ok=True)
+        file_path = folder_path / entry_title
+        print("")
+        print("Soda Says: " + random_greeting)
+        user_entry = input("Type Your Entry, Press Enter Once Finished: \n")
+        print("Entry Added to Diary!")
+        with open(file_path, 'w') as file:
+            file.write(user_entry)
+        print(f"File saved at: {file_path}")
+    else:
+        print("")
+        print("Soda Says: Please Enter a Valid Entry Title That Only Contains Letters.\n")
 
-    # list of Soda greetings, this will be randomized when called
-    greetings = [
-        "Hey! How’s it going?",
-        "Hi there! What’s up?",
-        "Hey! What’s new with you?",
-        "What’s going on?",
-        "Hi! Anything interesting happening?",
-        "Hi! Just checking in.",
-        "What’s happening? Keeping busy?",
-        "Hey! What’s the latest in your world?",
-        "What’s new? How’s everything going?",
-        "Hey! Hope everything's going alright.",
-        "What’s up? Just doing the usual?",
-        "Hi! Any news worth sharing?"
-    ]
+def choice_two():
+    desktop = Path.home() / 'Desktop'
+    folder_path = desktop / 'Diary Entries'
+    if not folder_path.exists():
+        print(f"The folder '{folder_path}' does not exist.")
+        print("Please Create a New Entry First!\n")
+    else:
+        for file_path in folder_path.iterdir():
+            if file_path.is_file():
+                print(f"Entry Title: {file_path.name}")
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                    print(f"Content: {content}")  # Print content
 
-    # What the user first sees when they start the program, this will not appear unless the program just launched
+def choice_three():
+    random_wisdom = random.choice(daily_wisdom)
     print("")
+    print("Soda Says: " + random_wisdom)
+    print("")
+
+def choice_four():
+    print("++++++++++++++++++++++COMMON QUESTIONS++++++++++++++++++++++")
+    print("Question: Where Are My Diary Entries!")
+    print("Soda Says: They should be on your desktop in a folder called 'Diary Entries'. Once you have opened that folder you can view all of the entries you have created!\n")
+    print("Question: How Do I Delete or Edit an Entry?")
+    print("Soda Says: The easiest way to do that would be to go to the 'Diary Entries' folder in your desktop and then either delete or edit the desired entry from there!\n")
+    print("Question: How Do I Contact the Creator of This Program?")
+    print("Soda Says: I believe the creator can be reached at suseyihzheen@gmail.com!")
+    print("++++++++++++++++++++++COMMON QUESTIONS++++++++++++++++++++++\n")
+
+def main():
+    art = """⢰⣾⣿⢶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣏⣿⣿
+⠸⣿⣿⣿⣇⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣿⣿⠿⠋
+⠀⠀⠉⢻⡅⠈⠉⠙⠓⠶⢦⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡤⠶⠚⠛⠉⠁⠠⣾⠇⠀⠀
+⠀⠀⠀⠈⣿⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠓⠲⠦⢤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠴⠶⠚⠋⠉⠁⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀
+⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠃⠀⠀⠀
+⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⣀⣀⣤⡴⠶⠶⠶⠶⠶⠦⣤⣀⣀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠉⠁⠀⠀⠂⠀⠀⠀⠀⠀⠒⠀⠉⠙⠛⢶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠈⠛⢶⣄⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢀⣴⠶⠛⠉⠉⠉⠛⠷⣤⡀⠀⠀⠀⠀⠙⢷⣄⠀⡀⠀⠀⢸⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⢡⣶⣶⡄⠀⠀⢠⡶⣮⢻⣆⠀⠀⠀⠀⠀⠻⣦⠀⠀⣸⡟⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣸⣿⣿⠃⠀⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠙⣷⠶⠿⠃⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠶⠶⣶⣶⡶⠄⠀⠀⠀⠀⠀⠀⠀⣀⡴⠾⠿⢯⣽⣿⣷⣤⢠⡆⠠⡄⣽⣿⣿⡿⠟⠛⢿⣆⠀⠀⠀⠻⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡿⠁⠀⠀⠀⠀⠀⠀⢀⣴⠟⠁⠀⠀⠀⠀⠀⠉⠉⠻⠿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⢙⢷⣄⠀⠀⢻⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⠀⢀⡴⠋⢀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣶⣶⡍⣈⣿⣆⠀⠼⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⢰⡟⠁⢀⣀⣸⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⢀⠀⢀⠀⠀⠀⢸⣿⣿⣿⣿⡅⠀⢻⡆⠀⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡆⠀⠀⠀⢰⡟⠀⠀⠀⠀⣿⣿⣿⣿⣿⡇⠀⠀⢀⠄⡰⠋⡴⠃⡴⠁⠀⠘⣿⣿⣿⣿⡇⠀⠀⣿⡞⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣧⠀⠀⠀⣾⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⠃⠀⠀⢀⠞⠀⠊⢀⣈⣀⠀⠀⠀⠙⠿⠿⠟⠀⠀⠀⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣎⠀⠀⣿⠀⠀⠀⠀⠀⠀⠙⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠘⠻⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⡂⣀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣾⢿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠖⠒⠒⠒⠒⠒⠒⠒⠒⠒⠶⠶⡦⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢯⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⣠⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣧⣄⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣷⠚⠁⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠛⢲⣴⣾⣿⡿⠿⣶⡶⠖⠉⠉⠉⠙⢦⠀⠀⠀⠀⠀⠀⣠⠞⠉⠙⠻⣿⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢶⠟⠁⢠⠏⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⠀⠙⣿⢿⡏⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⢸⠀⠛⠚⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠈⠦⣀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠦⣄⣀⡀⣀⣠⠴⠊⠳⠶⠶⠶⠶⠶⠶⠤⠤⠤⠤⠼⢹⠒⠒⠋⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢤⠤⠤⠤⣤⠀⠀⠀⢀⣿⠀⠉⠁⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"""
     print(art)
-    print("")
-    print("Soda Says: \n")
-    print("Welcome To Your Virtual Diary!\n")
-    print("Your Diary Entries Will Now Be Saved In A Folder Called 'Diary Entries' On Your Desktop\n")
-    print("My Name Is Soda! I Will Be Your Virtual Diary Assistant.\n")
-
-    # This only exits if the users enters 'Q' as an option
-    while not test_bool:
-        # main menu
+    game_loop = True
+  
+    while game_loop:
         print("****************Menu****************")
         print("1. New Entry")
         print("2. View Previous Entries")
@@ -155,88 +170,19 @@ def menu():
         print("4. FAQ")
         print("****************Menu****************")
         print("Press 'Q' to Exit\n")
-        user_input = input("Enter Your Choice: \n").lower()
-
-        # If user enters 1, Enter a new entry
-        if user_input == '1':
-            # Title for the Entry
-            entry_title = input("Enter Entry Title (Only Use Letters For Your Title):\n")
-            # If the entry title only contains letters then proceed
-            if entry_title.isalpha():
-                random_greeting = random.choice(greetings)
-                # Entry will save to the Desktop
-                desktop = Path.home() / 'Desktop'
-                # Entry will be saved as the entry title on the desktop
-                folder_path = desktop / 'Diary Entries'
-                folder_path.mkdir(exist_ok=True)
-                file_path = folder_path / entry_title
-                # Random greeting is given to the user
-                print("")
-                print("Soda Says: " + random_greeting)
-                # Asks the user to type out their entry
-                user_entry = input("Type Your Entry, Press Enter Once Finished: \n")
-                print("Entry Added to Diary!")
-                # Entry gets written to the entry title file into the diary entries folder
-                with open(file_path, 'w') as file:
-                    file.write(user_entry)
-                # Tells the user where the file is saved at
-                print(f"File saved at: {file_path}")
-
-            # Otherwise, don't let the user create a new entry.
-            else:
-                print("")
-                print("Soda Says: Please Enter a Valid Entry Title That Only Contains Letters.\n")
-        # If the user enters 2, then let them view their entries
-        elif user_input == '2':
-            # getting the path to the desktop
-            desktop = Path.home() / 'Desktop'
-            # specifying the folder we want to read out of
-            folder_path = desktop / 'Diary Entries'
-
-            # Check if the folder exists
-            if not folder_path.exists():
-                print(f"The folder '{folder_path}' does not exist.")
-                print("Please Create a New Entry First!\n")
-            else:
-                # List all files in the folder
-                for file_path in folder_path.iterdir():
-                    # making sure it's a file and not a subfolder
-                    if file_path.is_file():
-                        print("")
-                        # printing out all the entry titles in that folder
-                        print(f"Entry Title: {file_path.name}")
-                        # opening and reading all the files in the folder
-                        with open(file_path, 'r') as file:
-                            content = file.read()
-                            print(content)
-                            print("")
-
-        # If user input is 3, then give some random wisdom from the wisdom list
-        elif user_input == '3':
-            random_wisdom = random.choice(daily_wisdom)
-            print("")
-            print("Soda Says: " + random_wisdom)
-            print("")
-
-        # If user input is 4, then give the user the FAQ
-        elif user_input == '4':
-            print("")
-            print("++++++++++++++++++++++COMMON QUESTIONS++++++++++++++++++++++")
-            print("Question: Where Are My Diary Entries!")
-            print("Soda Says: They should be on your desktop in a folder called 'Diary Entries'. Once you have opened that folder you can view all of the entries you have created!\n")
-            print("Question: How Do I Delete or Edit an Entry?")
-            print("Soda Says: The easiest way to do that would be to go to the 'Diary Entries' folder in your desktop and then either delete or edit the desired entry from there!\n")
-            print("Question: How Do I Contact the Creator of This Program?")
-            print("Soda Says: I believe the creator can be reached at suseyihzheen@gmail.com!")
-            print("++++++++++++++++++++++COMMON QUESTIONS++++++++++++++++++++++\n")
-
-        # If user input is 'Q' or 'q', exit the program
-        elif user_input == 'q':
-            test_bool = True
-
-        # If user input is not 1-4 or 'Q', tell the user to enter a valid choice.
+        n = input("Enter Your Choice: \n").lower()
+        if n == '1':
+            choice_one()
+        elif n == '2':
+            choice_two()
+        elif n == '3':
+            choice_three()
+        elif n == '4':
+            choice_four()
+        elif n == 'q':
+            game_loop = False
         else:
-            print("")
-            print("Soda Says: Please Enter a Valid Choice between 1-4! Or Enter The Letter 'Q' to Exit.\n")
+            print("Please enter a valid choice!")
 
-menu()
+main()
+
